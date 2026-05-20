59c2c50669b273ab6796b0572922d8a7d86c755b
akinfalaolaide <horlaidef@gmail.com>
2026-05-20 17:51:55 +0100

    feat:dsa-commit

--- src/SortAndStar.java
+++ src/SortAndStar.java
@@ -3,25 +3,37 @@ public class SortAndStar {
     void main() {
         String[] laide = new String[]{"abeyyy", "Baller", "Abosktu"};
         System.out.println(twoSort(laide));
-
+        System.out.println(twoSort2(laide));
     }
 
     public static String twoSort(String[] s) {
         String smallest = s[0];
         for (String t : s) {
-            if(t.compareTo(smallest) < 0) {
+            if (t.compareTo(smallest) < 0) {
                 smallest = t;
             }
         }
         StringBuilder result = new StringBuilder();
         for (int j = 0; j < smallest.length(); j++) {
-            if(j > 0 ){
+            if (j > 0) {
                 result.append("***");
             }
             result.append(smallest.charAt(j));
 
         }
         return result.toString();
+
+    }
+
+    public static String twoSort2(String[] s) {
+        String smallest = s[0];
+        for (String t : s) {
+            if (t.compareTo(smallest) < 0) {
+                smallest = t;
+            }
+        }
+        //return smallest.replaceAll("([a-zA-Z])", "***$1").substring(3);
+        return String.join("***", smallest.split(""));
 
     }
 }
