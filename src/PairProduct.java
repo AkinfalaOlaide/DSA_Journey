5c53d29dfec345daea48148349506e265f553bc8
akinfalaolaide <horlaidef@gmail.com>
2026-07-27 23:41:15 +0100

    feat:dsa-commit

--- src/PairProduct.java
+++ src/PairProduct.java
@@ -1,20 +1,17 @@
 void main() {
-  //  System.out.println(pairProduct(List.of(3, 2, 5, 4, 1), 8));
+    System.out.println(pairProduct(List.of(3, 2, 5, 4, 1), 8));
     System.out.println(pairProduct(List.of(3, 2, 5, 4, 1), 10));
 }
 public static List<Integer> pairProduct(List<Integer> numbers, int target) {
-    HashMap<Integer, Integer> previousNums = new HashMap<>();
-    List<Integer> result = new ArrayList<>();
-    int complement;
-    for(int i = 0 ; i < numbers.size(); i++) {
+    HashMap<Double, Integer> previousNums = new HashMap<>();
+    double complement;
+    for(int i = 0 ; i < numbers.size(); i +=1) {
         int num = numbers.get(i);
-        complement = target/num;
-        if(!previousNums.containsKey(complement) || target % num != 0) {
-            previousNums.put(num , i);
-        }else{
-            result.add(previousNums.get(complement));
-            result.add(i);
+        complement = (double) target/num;
+        if(previousNums.containsKey(complement)) {
+           return List.of(previousNums.get(complement), i);
         }
+        previousNums.put((double) num ,i);
     }
-    return result;
+    return null;
 }
